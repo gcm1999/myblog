6c8e33f1cbbd06093c599ce0ac2826359b12d624
gcm <1453400328@qq.com>
2022-09-15 16:16:06 +0800

    1

--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-
-createApp(App).mount('#app')
+import { createApp } from "vue";
+import App from "./App.vue";
+import "@/assets/bootstrap/css/bootstrap.min.css";
+// import jQuery from "jquery";
+// import jQuery from "@/assets/jq/jQuery3.6.0"
+// import "@/assets/bootstrap/js/bootstrap";
+import "@/assets/bootstrap/js/bootstrap.min";
+const app = createApp(App);
+app.mount("#app");
