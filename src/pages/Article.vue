3fa48c3d211b2495bed57158411dd8a4edab6a5c
gcm <1453400328@qq.com>
2022-09-23 11:04:14 +0800

    留言板2.0

--- src/pages/Article.vue
+++ src/pages/Article.vue
@@ -2,12 +2,14 @@
   <h1>
     {{ $route.params.title }}
   </h1>
-  
 </template>
 <script>
+import router from "@/router";
 export default {
   name: "Article",
-  setup() {},
+  setup() {
+    console.log(router);
+  },
 };
 </script>
 <style scoped></style>
--- src/pages/MessageBoard.vue
+++ src/pages/MessageBoard.vue
@@ -1,23 +1,38 @@
 <template>
   留言板
   <!-- {{ data.messageList }} -->
+  <div class="msg-box clearFix">
+    <div>
+      <input type="text" v-model="user" class="user" placeholder="用户名" />
+    </div>
+    <div>
+      <textarea type="text" v-model="message" class="user" placeholder="留言内容" />
+    </div>
+    <button @click="setMessage">提交</button>
+  </div>
+
   <ul class="list-group">
-    <li class="list-group-item">
+    <!-- <li class="list-group-item">
       用户名:<input type="text" v-model="user" /> 留言内容:<input
         type="text"
         v-model="message"
       />
 
       <input type="button" value="提交" @click="setMessage" />
-    </li>
+    </li> -->
     <li
       class="list-group-item"
       v-for="(msg, index) in messageList"
       :key="msg.id"
     >
-      <span class="badge">{{ timeHandler(msg.time) }}</span>
+      <!-- <span class="badge">{{ timeHandler(msg.time) }}</span>
       {{ msg.user }}:
-      {{ msg.msg }}
+      {{ msg.msg }} -->
+      <div class="msg" :username="msg.user.slice(0, 1)">
+        <span>{{ msg.user }}</span>
+        <span>{{ timeHandler(msg.time) }}</span>
+        <div>{{ msg.msg }}</div>
+      </div>
     </li>
   </ul>
 </template>
@@ -62,4 +77,54 @@ export default {
   },
 };
 </script>
-<style scoped></style>
+<style lang="stylus" scoped>
+.msg:before
+  content attr(username)
+  text-align center
+  line-height 50px
+  font-size 36px
+  border-radius 50%
+  display inline-block
+  width 50px
+  height 50px
+  position absolute
+  background-color pink
+  left -50px
+  top 2px
+.msg
+  display flex
+  flex-direction column
+  margin-left 50px
+  position relative
+  span
+    margin-left 10px
+  span:nth-of-type(1)
+    font-size 24px
+  span:nth-of-type(2)
+    font-size 12px
+    color #666
+  div
+    margin 10px
+    background-color: #eee;
+    padding 10px
+    font-size 16px
+
+.msg-box
+  padding: 10px;
+  background-color: #eee;
+  div
+    padding 10px
+    // background-color white
+    .user
+      // background-color: #eee;
+      outline none
+      border 1px solid black
+    textarea
+      width 100%
+      height 100px
+  button
+    float right
+    padding 3px 5px
+    background-color white
+    border 0
+</style>
